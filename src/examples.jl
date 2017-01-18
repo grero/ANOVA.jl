cf9311a01a7bd218ba8705e13a362a31a6a40e89
roger <roger.herikstad@gmail.com>
2017-01-18 10:30:23 +0800

    Adds docstring

--- src/examples.jl
+++ src/examples.jl
@@ -1,3 +1,6 @@
+"""
+Example data taken from http://www.statsdirect.com/help/analysis_of_variance/two_way_replicate.htm
+"""
 function example1()
 	X2 = [9.8, 9.9,11.3, 10.1, 9.5, 10.7, 9.8, 10.0, 10.7, 9.2, 9.1, 10.3, 8.6, 9.1, 10.7, 9.2, 9.4, 10.2, 8.4, 8.6, 9.8, 7.9, 8.0, 10.1, 8.0, 8.0, 10.1]
 	treatment = repmat(["A", "B", "C"],9,1)
