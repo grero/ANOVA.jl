df4e38c36788190f2963ac554cd3dc19b1d7541f
roger <roger.herikstad@gmail.com>
2017-01-18 10:56:08 +0800

    Fixes deprecations

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,8 +17,10 @@ end
 function test_anova()
 	#data taken from https://people.richland.edu/james/ictcm/2004/twoway.html
 	Y = [54, 49, 59, 39, 55, 25, 29, 47, 26, 28,53, 72, 43, 56, 52,46, 51, 33, 47, 41,33, 30, 26, 25, 29,18, 21, 34, 40, 24]
-	race = repmat(repmat(["Caucasian", "African-American", "Hispanic"], 1,5)'[:]',2,1)[:]
-	gender = repmat(repmat(["Male","Female"], 1,5)'[:],3,1)
+  _race = ["Caucasian", "African-American", "Hispanic"]
+  race = [r for i in 1:10, r in _race][:]
+  G = ["Male", "Female"]
+  gender = [g for i = 1:5, g in G,j in 1:3][:]
 	P = ANOVA.anova(Y, race, gender)
 	@test_approx_eq P.SS_1 2328.2
 	@test_approx_eq P.SS_2 907.5
